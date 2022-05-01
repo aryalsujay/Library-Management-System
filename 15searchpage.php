13758994f2f6404c9785c2408e2c9361fa516397
aryalsujay <67737569+aryalsujay@users.noreply.github.com>
2022-05-01 09:30:15 +0530

    Delete 15searchpage.php

--- 15searchpage.php
+++
@@ -1,9 +0,0 @@
-<?php include "5data_class.php";
-$search_keyword = '';
-if(!empty($_POST['search']['keyword'])) {
-    $search_keyword = $_POST['search']['keyword'];
-}
-$obj=new data();
-$obj->setconnection();
-$obj->search($search_keyword);
-?>
